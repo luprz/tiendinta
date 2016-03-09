f3ff4a38b0ebb49d0429f4ed8c972302ba1b1123
Your Name <you@example.com>
2016-03-09 14:18:01 -0430

    My initial commit message

---
+++ config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', '192.168.1.190');
+define('_DB_NAME_', 'prestashopdb');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'dbslicetech');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'iaf2CL6ejkZAEIKiUb66veLsOU1mIYRBq005irSqSW0ZRPaGL1j3wZcd');
+define('_COOKIE_IV_', 'S3r8hMKh');
+define('_PS_CREATION_DATE_', '2016-03-07');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.4');
